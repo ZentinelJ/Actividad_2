fadc5c8c01cb8377664a244f976c079c8f2ce78c
villa <@ZentinelJ>
2025-05-23 09:24:36 -0500

    Se crea clase Personas con las variables privadas y los gets

---
+++ src/Personas.java
@@ -0,0 +1,26 @@
+public class Personas {
+        private class String nombre;
+        private class String apellido;
+        private class string genero;
+        private class int edad;
+
+        public Personas (string nombre, string apellido, string genero, int edad) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.genero = genero;
+        }
+
+        public  string getNombre () {
+            return nombre;
+        }
+
+        public  string getGenero() {
+            return genero;
+        }
+
+        public string getEdad () {
+            return  edad;
+        }
+    }
+
+}
